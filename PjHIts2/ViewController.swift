027c5cb3f355b83cf1898dec83d2a3ccec6a0692
eamozart0122@gmail.com <eamozart0122@gmail.com>
2022-11-03 13:57:06 +0900

    Initial Commit

---
+++ PjHIts2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PjHIts2
+//
+//  Created by 阿久津栄一 on 2022/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
