a3528690f1ce85c51a6c11a5acfa34a3f7fc77b1
cookiedragonsoul <cookiedragonsoul@gmail.com>
2022-11-03 18:21:20 +0900

    2

--- PjHIts2/ViewController.swift
+++ PjHIts2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("ohkiTest")
+        print("ohkiTest2")
     }
 
 
